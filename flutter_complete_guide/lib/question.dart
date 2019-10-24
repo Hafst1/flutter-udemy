a842e1ba0108b772e7c97e91dfc8b9cf80cfc849
Hafsteinn <hafsteinn24@gmail.com>
2019-10-24 22:47:52 +0000

    flutter and firebase project from youtube

--- flutter_complete_guide/lib/question.dart
+++ flutter_complete_guide/lib/question.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String questionText;
+  final String questionText;
 
   Question(this.questionText);
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Text(
+      questionText,
+      style: TextStyle(fontSize: 28),
+    );
   }
 }
