38237e4e2a4c56394f434ecf138bfa997b19ed23
Hafsteinn <hafsteinn24@gmail.com>
2019-11-08 23:43:57 +0000

    adding a loading extension to sign in and register

--- brew_crew/lib/screens/authenticate/register.dart
+++ brew_crew/lib/screens/authenticate/register.dart
@@ -1,11 +1,12 @@
-import 'package:brew_crew/shared/constants.dart';
-import 'package:flutter/material.dart';
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
+import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
 
   final Function toggleView;
-  Register({ this.toggleView});
+  Register({ this.toggleView });
 
   @override
   _RegisterState createState() => _RegisterState();
@@ -15,15 +16,16 @@ class _RegisterState extends State<Register> {
 
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  String error = '';
+  bool loading = false;
 
   // text field state
   String email = '';
   String password = '';
-  String error = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return loading ? Loading() : Scaffold(
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
@@ -33,11 +35,9 @@ class _RegisterState extends State<Register> {
           FlatButton.icon(
             icon: Icon(Icons.person),
             label: Text('Sign In'),
-            onPressed: () {
-              widget.toggleView();
-            },
-          )
-        ] 
+            onPressed: () => widget.toggleView(),
+          ),
+        ],
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
@@ -51,16 +51,16 @@ class _RegisterState extends State<Register> {
                 validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() => email = val);
-                }
+                },
               ),
               SizedBox(height: 20.0),
               TextFormField(
                 decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                validator: (val) => val.length < 6 ? 'Enter a password 6+ chars long' : null,
                 obscureText: true,
+                validator: (val) => val.length < 6 ? 'Enter a password 6+ chars long' : null,
                 onChanged: (val) {
                   setState(() => password = val);
-                }
+                },
               ),
               SizedBox(height: 20.0),
               RaisedButton(
@@ -70,22 +70,26 @@ class _RegisterState extends State<Register> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                   if (_formKey.currentState.validate()){
-                     dynamic result = await _auth.registerWithEmailAndPassword(email, password);
-                     if (result == null) {
-                       setState(() => error = 'please supply a valid email');
-                     }
-                   }
-                },
+                  if(_formKey.currentState.validate()){
+                    setState(() => loading = true);
+                    dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                    if(result == null) {
+                      setState(() {
+                        loading = false;
+                        error = 'Please supply a valid email';
+                      });
+                    }
+                  }
+                }
               ),
               SizedBox(height: 12.0),
               Text(
                 error,
-                style: TextStyle(color: Colors.red, fontSize:14.0),
-              ),
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
+              )
             ],
           ),
-        )
+        ),
       ),
     );
   }
--- brew_crew/lib/screens/authenticate/sign_in.dart
+++ brew_crew/lib/screens/authenticate/sign_in.dart
@@ -1,11 +1,12 @@
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
 
   final Function toggleView;
-  SignIn({ this.toggleView});
+  SignIn({ this.toggleView });
 
   @override
   _SignInState createState() => _SignInState();
@@ -15,29 +16,28 @@ class _SignInState extends State<SignIn> {
 
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  String error = '';
+  bool loading = false;
 
   // text field state
   String email = '';
   String password = '';
-  String error = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return loading ? Loading() : Scaffold(
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
-        title: Text('Sign in to Brew Crew'), 
+        title: Text('Sign in to Brew Crew'),
         actions: <Widget>[
           FlatButton.icon(
             icon: Icon(Icons.person),
             label: Text('Register'),
-            onPressed: () {
-              widget.toggleView();
-            },
-          )
-        ]
+            onPressed: () => widget.toggleView(),
+          ),
+        ],
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
@@ -51,16 +51,16 @@ class _SignInState extends State<SignIn> {
                 validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() => email = val);
-                }
+                },
               ),
               SizedBox(height: 20.0),
               TextFormField(
+                obscureText: true,
                 decoration: textInputDecoration.copyWith(hintText: 'Password'),
                 validator: (val) => val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                obscureText: true,
                 onChanged: (val) {
                   setState(() => password = val);
-                }
+                },
               ),
               SizedBox(height: 20.0),
               RaisedButton(
@@ -70,22 +70,26 @@ class _SignInState extends State<SignIn> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                   if (_formKey.currentState.validate()){
-                     dynamic result = await _auth.signInWithEmailAndPassword(email, password);
-                     if (result == null) {
-                       setState(() => error = 'Could not sign in with those credential ');
-                     }
-                   }
-                },
+                  if(_formKey.currentState.validate()){
+                    setState(() => loading = true);
+                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
+                    if(result == null) {
+                      setState(() {
+                        loading = false;
+                        error = 'Could not sign in with those credentials';
+                      });
+                    }
+                  }
+                }
               ),
               SizedBox(height: 12.0),
               Text(
                 error,
-                style: TextStyle(color: Colors.red, fontSize:14.0),
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
               ),
             ],
           ),
-        )
+        ),
       ),
     );
   }
